effed6f8d151a358dddef273cadbb3df0b709e25
Vasily Makarov <vasily.makarov@ultratendency.com>
2022-02-06 15:27:45 +0300

    start filling up

--- src/components/Home/Intro.vue
+++ src/components/Home/Intro.vue
@@ -3,7 +3,6 @@
         <div class="static-container">
             <h1 class="title">
                 <span class="iuri">agentdalecoper</span>
-                <span class="func">.github.io</span>
             </h1>
 
             <div class="std">
